6e179791523560d942a22daa849daff2b72c95cf
felipeam10 <felipeam10@hotmail.com>
2024-08-19 02:37:15 -0300

    vJSR-9.19 - GET user posts

--- src/main/java/com/devsuperior/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/workshopmongo/repositories/PostRepository.java
@@ -1,6 +1,7 @@
 package com.devsuperior.workshopmongo.repositories;
 
 import com.devsuperior.workshopmongo.entities.Post;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
@@ -10,6 +11,8 @@ import java.util.List;
 
 public interface PostRepository extends ReactiveMongoRepository<Post, String>{
 	
+	@Query("{ 'user' : ?0 }")
+	Flux<Post> findByUser(ObjectId id);
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	Flux<Post> searchTitle(String text);
 	
